6f9c2b8938ad03bd74735eccf6024e214fd47b29
Jelly Terra <jellyterra@proton.me>
2024-09-03 12:50:46 +0800

    [builtin] Add almost all functions from Go std library to API: `strings`,`path`

--- structure/v1_path.go
+++ structure/v1_path.go
@@ -5,9 +5,30 @@
 package structure
 
 import (
-	"path"
+	. "path"
 )
 
 type v1Path struct{}
 
-func (v1Path) Join(paths ...string) string { return path.Join(paths...) }
+func (v1Path) Base(path string) string { return Base(path) }
+
+func (v1Path) Clean(path string) string { return Clean(path) }
+
+func (v1Path) Dir(path string) string { return Dir(path) }
+
+func (v1Path) Ext(path string) string { return Ext(path) }
+
+func (v1Path) IsAbs(path string) bool { return IsAbs(path) }
+
+func (v1Path) Join(paths ...string) string { return Join(paths...) }
+
+func (v1Path) Match(pattern, name string) (matched bool, valid bool) {
+	matched, err := Match(pattern, name)
+	if err != nil {
+		return false, false
+	}
+
+	return matched, true
+}
+
+func (v1Path) Split(path string) (dir, file string) { return Split(path) }
--- structure/v1_strings.go
+++ structure/v1_strings.go
@@ -4,9 +4,52 @@
 
 package structure
 
-import "strings"
+import . "strings"
 
 type v1Strings struct{}
 
-func (v1Strings) HasPrefix(str string, prefix string) bool    { return strings.HasPrefix(str, prefix) }
-func (v1Strings) TrimPrefix(str string, prefix string) string { return strings.TrimPrefix(str, prefix) }
+func (v1Strings) Contains(s, substr string) bool     { return Contains(s, substr) }
+func (v1Strings) ContainsAny(s, chars string) bool   { return Contains(s, chars) }
+func (v1Strings) ContainsRune(s string, r rune) bool { return ContainsRune(s, r) }
+
+func (v1Strings) Count(s, substr string) int { return Count(s, substr) }
+
+func (v1Strings) HasPrefix(str, prefix string) bool { return HasPrefix(str, prefix) }
+func (v1Strings) HasSuffix(str, suffix string) bool { return HasSuffix(str, suffix) }
+
+func (v1Strings) Index(s, substr string) int     { return Index(s, substr) }
+func (v1Strings) IndexAny(s, chars string) int   { return IndexAny(s, chars) }
+func (v1Strings) IndexByte(s string, c byte) int { return IndexByte(s, c) }
+func (v1Strings) IndexRune(s string, r rune) int { return IndexRune(s, r) }
+
+func (v1Strings) Join(sep string, elems ...string) string { return Join(elems, sep) }
+
+func (v1Strings) LastIndex(s, substr string) int     { return LastIndex(s, substr) }
+func (v1Strings) LastIndexAny(s, chars string) int   { return LastIndexAny(s, chars) }
+func (v1Strings) LastIndexByte(s string, c byte) int { return LastIndexByte(s, c) }
+
+func (v1Strings) Repeat(s string, count int) string { return Repeat(s, count) }
+
+func (v1Strings) Replace(s, old, new string, n int) string { return Replace(s, old, new, n) }
+func (v1Strings) ReplaceAll(s, old, new string) string     { return ReplaceAll(s, old, new) }
+
+func (v1Strings) Split(s, substr string) []string      { return Split(s, substr) }
+func (v1Strings) SplitAfter(s, substr string) []string { return SplitAfter(s, substr) }
+func (v1Strings) SplitAfterN(s, substr string, n int) []string {
+	return SplitAfterN(s, substr, n)
+}
+func (v1Strings) SplitN(s, substr string, n int) []string { return SplitN(s, substr, n) }
+
+func (v1Strings) ToLower(s string) string { return ToLower(s) }
+func (v1Strings) ToTitle(s string) string { return ToTitle(s) }
+func (v1Strings) ToUpper(s string) string { return ToUpper(s) }
+func (v1Strings) ToValidUTF8(s, replacement string) string {
+	return ToValidUTF8(s, replacement)
+}
+
+func (v1Strings) Trim(s, cutset string) string         { return Trim(s, cutset) }
+func (v1Strings) TrimLeft(s, cutset string) string     { return TrimLeft(s, cutset) }
+func (v1Strings) TrimPrefix(str, prefix string) string { return TrimPrefix(str, prefix) }
+func (v1Strings) TrimRight(s, cutset string) string    { return TrimRight(s, cutset) }
+func (v1Strings) TrimSpace(s string) string            { return TrimSpace(s) }
+func (v1Strings) TrimSuffix(str, suffix string) string { return TrimSuffix(str, suffix) }
